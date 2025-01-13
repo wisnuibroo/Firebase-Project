a6bdb994cb6b867eacec766c405f56808a7f1ec9
wisnuibroo <wisnuaja0999@gmai.com>
2025-01-13 19:50:17 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_project/route/approute.dart';
+import 'package:firebase_project/route/myapproute.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
